7011b473bbb017211eae916af45561c14d24a464
ediellm <ediellandulfo@gmail.com>
2021-07-07 01:21:19 +0000

    App Python v1.0

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print("Hello World")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_inicial():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # Ajuste a linha abaixo em seu código
+
+    
